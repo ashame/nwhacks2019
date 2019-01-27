8c2e93d282945a7dc4d1f4060e38ded0afe36cb4
Gina <1ginahong@gmail.com>
2019-01-27 11:02:55 -0800

    text changes and some css changes

--- src/components/pages/FollowTheStars.jsx
+++ src/components/pages/FollowTheStars.jsx
@@ -10,43 +10,43 @@ class FollowTheStars extends Component {
             this.rChange = 0.015;
             this.color = color;
         }
-        
+
         Star.prototype = {
             constructor: Star,
             render: function(){
               context.beginPath();
               context.arc(this.x, this.y, this.r, 0, 2*Math.PI, false);
-              context.shadowBlur = 8; 
+              context.shadowBlur = 8;
               context.shadowColor = "white";
               context.fillStyle = this.color;
               context.fill();
             },
             update: function(){
-              
+
                if (this.r > 2 || this.r < .8){
                    this.rChange = - this.rChange;
                }
                this.r += this.rChange;
             }
         }
-        
+
         var canvas = document.getElementById("canvas");
         var context = canvas.getContext("2d");
-        
+
         var C_WIDTH = canvas.width = document.body.offsetWidth;
         var C_HEIGHT = canvas.height = document.body.offsetHeight;
-        
+
         function randomColor(){
                 var arrColors = ["ffffff", "ffecd3" , "bfcfff"];
                 return "#"+arrColors[Math.floor((Math.random()*3))];
         }
-                
+
         var arrStars = [];
         for(var i = 0; i < 400; i++){
             var randX = Math.floor((Math.random()*C_WIDTH)+1);
             var randY = Math.floor((Math.random()*C_HEIGHT)+1);
             var randR = Math.random() * 1.7 + .5;
-            
+
             var star = new Star(randX, randY, randR, randomColor());
             arrStars.push(star);
         }
@@ -65,16 +65,16 @@ class FollowTheStars extends Component {
             requestAnimationFrame(animate);
 
             context.fillStyle = "white";
-            context.font = "30px Arial";
-            context.fillText("“Mom told me to follow the stars”", 35, 60);   
+            context.font = "30px 'Press Start 2P'";
+            context.fillText("“Mom told me to follow the stars”", canvas.height / 4, canvas.height / 2);
         }
-        
+
         animate();
     }
     render(){
         return(
         <canvas id="canvas" style={{background: "black",position: "absolute"}}></canvas>
-    
+
         )
     }
 }
