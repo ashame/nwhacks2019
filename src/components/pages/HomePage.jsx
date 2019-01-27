805d2bc0f3105572793dd32480c45e987b55bea2
ashame <nathan@imashamed.net>
2019-01-27 07:23:08 -0800

    logo, cleanup

--- src/components/pages/HomePage.jsx
+++ src/components/pages/HomePage.jsx
@@ -4,10 +4,10 @@ class HomePage extends Component {
     render() { 
         return(
             <div>
-                <p class="logo">
-                    Blank is a story about finding home
-                </p>
-                <button onClick={this.props.handleClick}>Play</button>
+                <div class="logo">
+                    <img src={require('../../assets/logo.png')} alt="logo" height="250" />
+                </div>
+                <button className="play" onClick={this.props.handleClick}>Play!</button>
             </div>
         )
     }
